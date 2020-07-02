39a758b48d8e935ee1f955aa2539f1a6db0d7f6d
Zhuk Oxana <58565993+oxanazhuk@users.noreply.github.com>
2020-07-02 15:58:45 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# goit-js-hw-8
+# goit-js-hw-8
---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+import images from "./gallery.js";
+
+
+const ul = document.querySelector(".js-gallery");
+const lightbox = document.querySelector(".js-lightbox");
+const lightboxImage = document.querySelector(".js-lightbox-image");
+const closeBtn = document.querySelector(".js-close-lightbox");
+
+// Деструктуризация объекта из images
+const buildImage = ({
+  preview,
+  original,
+  description
+}) => `
+<li class="gallery__item">
+  <a class="gallery__link">
+     <img class="gallery__image" 
+       src="${preview}" 
+       alt="${description}"
+       data-original="${original}"/>
+  </a>
+</li>
+`;
+
+const layoutImages = images.map(image => buildImage(image));
+ul.innerHTML = layoutImages.join('');
+
+
+ul.addEventListener('click', e => {
+  lightbox.classList.add('is-open');
+  lightboxImage.src = e.target.dataset.original;
+});
+
+closeBtn.addEventListener('click', () => {
+  lightbox.classList.remove('is-open');
+  lightboxImage.src = '';
+})
